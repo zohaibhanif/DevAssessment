de0f37e463519cb1f0e0e719d779ba1f3d3e64f3
Zohaib Hanif <zohaib@avantipoint.com>
2019-10-25 13:29:13 +0500

    Added circle image
    - Added profile circle image.
    
    fixes #9

--- src/DevAssessment.Android/Resources/Resource.designer.cs
+++ src/DevAssessment.Android/Resources/Resource.designer.cs
@@ -5793,6 +5793,9 @@ namespace DevAssessment.Droid
 			// aapt resource value: 0x7F070075
 			public const int tooltip_frame_light = 2131165301;
 			
+			// aapt resource value: 0x7F070076
+			public const int user_icon = 2131165302;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
